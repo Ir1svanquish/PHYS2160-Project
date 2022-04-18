1211df8152d836ba560585d18138662b5044c273
Ir1svanquish <zhijieivan@gmail.com>
2022-04-18 23:13:31 +0800

    Modified four files.

--- ep_ring.py
+++ ep_ring.py
@@ -16,27 +16,24 @@ density = Q / (2 * math.pi * R)
 numerical = []
 
 
-def func():
+def func(x):
     return 1
 
 
 for i in range(100):
     constant = 1 / (4 * math.pi * epsilon_0) * density * R / math.sqrt(R ** 2 + z[i] ** 2)
     numerical.append(constant * quad(func, 0, 2 * math.pi)[0])
-
-print(numerical)
 numerical = np.asarray(numerical)
 
 analytical = []
 for j in range(100):
     analytical.append(Q / ((4 * math.pi * epsilon_0) * math.sqrt(z[j] ** 2 + R ** 2)))
-print(analytical)
 analytical = np.asarray(analytical)
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
 ax.plot(z, numerical, c="b", label="Numerical solution")
-ax.plot(z, analytical, c="r", label="analytical solution")
+ax.plot(z, analytical, c="r", ls=":", lw=5, label="analytical solution")
 ax. set_xlabel("z")
 ax. set_ylabel("V(z)")
 ax. set_title("V(z) against z")
